aea3a92754f734262053609163efd61f04097d88
seanaleid <seanaleid@gmail.com>
2019-09-24 17:59:27 -0400

    SignUp and SignIn pages routed and functional on the navbar

--- cookbook/src/App.js
+++ cookbook/src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import MainFeed from "./components/features/MainFeed";
 
+import MainFeed from "./components/features/MainFeed";
+import SignIn from "./components/features/SignIn";
+import SignUp from "./components/features/SignUp";
 
 function App() {
   return (
     <Router>
       <div className="App">
     
-       <Route exact path='/' component={MainFeed} />
-  
+      <Route exact path='/' component={MainFeed} />
+      <Route path="/sign-in" render={props => <SignIn {...props}/>} />
+      <Route path="/sign-up" render={props => <SignUp {...props}/>} /> 
       </div>
     </Router>
     
