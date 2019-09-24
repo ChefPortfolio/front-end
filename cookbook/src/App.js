99c8735d9ce3024831abc0e9e3dfcd554415e82e
Justin Kane <JustinKane197@gmail.com>
2019-09-24 15:21:09 -0400

    navigation template added

--- cookbook/src/App.js
+++ cookbook/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 
+import Navigation from './components/features/Navigation';
 import SignUp from './components/features/SignUp';
 import SignIn from './components/features/SignIn';
 import UserInfo from "./components/features/UserInfo";
@@ -11,7 +12,7 @@ function App() {
   return (
     <div className="App">
 
-
+    <Navigation />
     <SignUp />
     <UserInfo />
     <SignIn />
