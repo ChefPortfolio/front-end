347efa988185766c32f004586a66b35ee6a661b3
Mark Gowen <mark.gowen78@gmail.com>
2019-09-25 10:12:57 -0700

    updated RecipeCard and MainFeed.js
    updated RecipeCard with props and added to MainFeed.js
    
    Co-Authored-By: Sean Naleid <seanaleid@users.noreply.github.com>
    Co-Authored-By: Justin Kane <justin-kane@users.noreply.github.com>

--- cookbook/src/components/features/MainFeed.js
+++ cookbook/src/components/features/MainFeed.js
@@ -1,14 +1,48 @@
 import React from "react";
+import RecipeCard from '../crud/RecipeCard'
 
 import Navigation from "./Navigation";
 
-
+const DummyData = [
+    {
+      "title": "Hot Ham and Cheese Sandwiches",
+      "description": "Do not settle for ordinary ham and cheese sandwiches...",
+      "instructions": "Preheat oven to 250 degrees F (120 degrees C)...",
+      "meal_type": "lunch",
+      "pic_url": ""
+    },
+    {
+      "title": "Old Fashioned Pancakes",
+      "description": "This is a great recipe...",
+      "instructions": "In a large bowl, sift together...",
+      "meal_type": "breakfast",
+      "pic_url": ""
+    },
+    {
+      "title": "Gouda and Spinach Stuffed Pork Chops",
+      "description": "This turned out absolutely delicious!!",
+      "instructions": "Preheat the oven to 400 degrees...",
+      "meal_type": "dinner",
+      "pic_url": ""
+    },
+  ]
 
 export default function MainFeed(){
 
     return(
         <div className="MainFeedWrapper">
             <Navigation />
+            {DummyData.map(recipe => {
+                return (
+                    <RecipeCard key={recipe.id}
+                    title={recipe.title}
+                    subheader={recipe.meal_type}
+                    image={recipe.pic_url}
+                    description={recipe.description}
+                    instructions={recipe.instructions}
+                    />
+                )
+            })}
         </div>
     )
 }
