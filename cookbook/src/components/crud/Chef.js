c8b838c127f216e0643d05ac9900f77fb7999aad
seanaleid <seanaleid@gmail.com>
2019-09-26 12:40:34 -0400

    Added BellIcon.js and ChatIcon.js, start styling Chef.js
    We added the two icon components to the logo folder and have started styling Chef.js using styled components.
    
    Co-Authored-By: Justin Kane <justin-kane@users.noreply.github.com>
    Co-Authored-By: Mark Gowen <markgowen@users.noreply.github.com>

--- cookbook/src/components/crud/Chef.js
+++ cookbook/src/components/crud/Chef.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import styled from "styled-components";
+import ChatIcon from "../logos/ChatIcon";
+import BellIcon from "../logos/BellIcon";
 
 // import modal
 import PostModal from './PostModal';
+
+const StyledAvatar= styled.div`
+`;
+
+const StyledName= styled.h1`
+`;
+
+const StyledInfo= styled.p`
+`;
+
+const StyledModal= styled.div`
+`;
+
 
 const Chef = props => {
     return (
         <>
             <div className="post-card">
                 {console.log('Checking props', props)}
+                <BellIcon />
+                <ChatIcon />
                 <h2>{props.chef.first_name}</h2>
                 <h2>{props.chef.last_name}</h2>
                 <p>{props.chef.location}</p>
