4bed37a97e18bd893ef2e3bf92f1de9f07baf541
Mark Gowen <mark.gowen78@gmail.com>
2019-09-25 17:38:45 -0700

    Routing for SignUp and UserInfo
    created link between SignUp.js and UserInfo
    
    Co-Authored-By: Sean Naleid <seanaleid@users.noreply.github.com>
    Co-Authored-By: Justin Kane <justin-kane@users.noreply.github.com>

--- cookbook/src/App.js
+++ cookbook/src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import MainFeed from "./components/features/MainFeed";
 import SignIn from "./components/features/SignIn";
 import SignUp from "./components/features/SignUp";
+import UserInfo from './components/features/UserInfo'
 import ChefPortfolio from './components/crud/ChefPortfolio';
 
 
@@ -30,6 +31,7 @@ function App() {
       <Route exact path='/' component={MainFeed} />
       <Route path="/sign-in" render={props => <SignIn {...props}/>} />
       <Route path="/sign-up" render={props => <SignUp {...props}/>} /> 
+      <Route path="/user-info" render={props => <UserInfo {...props}/>} /> 
 
       {/* this will be turned into a Private Route - Alexis */}
       <Route path='/chefportfolio' component={ChefPortfolio} />
