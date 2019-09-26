6074e094d8549441db4728cac2b60a1b27b3f5ca
seanaleid <seanaleid@gmail.com>
2019-09-26 17:13:15 -0400

    Replaced dummydata with chef for .maps
    Took out instances of dummy data on chefPortfolio.js and replaced with chef
    
    Co-Authored-By: Justin Kane <justin-kane@users.noreply.github.com>
    Co-Authored-By: Mark Gowen <markgowen@users.noreply.github.com>

--- cookbook/src/components/crud/ChefPortfolio.js
+++ cookbook/src/components/crud/ChefPortfolio.js
@@ -26,38 +26,6 @@ const useStyles = makeStyles(theme => ({
 
   }));
 
-const dummyChef = {
-  first_name: 'Tom',
-  last_name: 'Tom',
-  location: 'Tomtown',
-  contact: '111',
-  username: 'tomtom'
-};
-
-const dummyRecipes = [
-  {
-    title: 'Hot Ham and Cheese Sandwiches',
-    description: 'Do not settle for ordinary ham and cheese sandwiches...',
-    instructions: 'Preheat oven to 250 degrees F (120 degrees C)...',
-    meal_type: 'lunch',
-    pic_url: ''
-  },
-  {
-    title: 'Old Fashioned Pancakes',
-    description: 'This is a great recipe...',
-    instructions: 'In a large bowl, sift together...',
-    meal_type: 'breakfast',
-    pic_url: ''
-  },
-  {
-    title: 'Gouda and Spinach Stuffed Pork Chops',
-    description: 'This turned out absolutely delicious!!',
-    instructions: 'Preheat the oven to 400 degrees...',
-    meal_type: 'dinner',
-    pic_url: ''
-  }
-];
-
 const ChefPortfolio = () => {
     const classes = useStyles();
   const [chef, setChef] = useState([]);
@@ -76,7 +44,7 @@ const ChefPortfolio = () => {
 
     //* When backend delivers an id, need to get id to localStorage instead of hard code */
     axiosWithAuth()
-      .get(`https://chefbook-stacy.herokuapp.com/api/chefs/${id}/recipes`)
+      .get(`https://chefbook-stacy.herokuapp.com/api/recipes`)
       .then(res => {
         console.log('GET request for chefs', res);
         setChef(res.data);
@@ -97,7 +65,7 @@ const ChefPortfolio = () => {
 
     //* Need to grab from localStorage */
 
-    const id = '1';
+    // const id = '1';
     axiosWithAuth()
       .post(`https://chefbook-stacy.herokuapp.com/api/recipes/`, newRecipe)
       .then(res => {
@@ -133,12 +101,12 @@ const ChefPortfolio = () => {
                     <button type="submit" onClick={addRecipe}>Submit</button>
             </form> */}
       <Navigation />
-      <Chef chef={dummyChef} />
+      <Chef chef={chef} />
       {/* {chef.recipes.map(recipe => {
                         return <RecipeCard props={chef.recipes} />
                     })} */}
       <Grid container className={classes.root} justify="center">
-        {dummyRecipes.map(recipe => {
+        {chef.map(recipe => {
           return (
             <Grid item className={classes.gridItem} s>
               <RecipeCard
@@ -154,7 +122,7 @@ const ChefPortfolio = () => {
           );
         })}
       </Grid>
-      {/* {dummyRecipes.map(recipe => {
+      {/* {chef.map(recipe => {
         return (
           <RecipeCard
             title={recipe.title}
